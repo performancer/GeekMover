52ff25b74f583eff84425cff26a1ce6882d32912
lauri <lauri.lundell@outlook.com>
2019-04-28 13:58:32 +0300

    DaySerialization-luokka joka ei valmis

--- app/src/main/java/com/example/geekmover/UserData.java
+++ app/src/main/java/com/example/geekmover/UserData.java
@@ -1,7 +1,9 @@
 package com.example.geekmover;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.JsonWriter;
+import android.util.Log;
 
 import com.example.geekmover.data.Day;
 import com.example.geekmover.data.Exercise;
@@ -71,47 +73,4 @@ public class UserData {
         editor.putInt("weight", weight);
         editor.apply();
     }
-
-    public void writeJsonStream(OutputStream out, List<Day> days) throws IOException{
-        JsonWriter writer = new JsonWriter(new OutputStreamWriter(out, "UTF-8"));
-        writer.setIndent("  ");
-        writeDaysArray(writer, days);
-        writer.close();
-    }
-
-    private void writeDaysArray(JsonWriter writer, List<Day> days) throws IOException {
-        writer.beginArray();
-        for(Day day : days){
-            writeDay(writer, day);
-        }
-        writer.endArray();
-    }
-
-    private void writeDay(JsonWriter writer, Day day) throws IOException {
-        writer.beginObject();
-        writer.name("date").value(day.getDate().getTime());
-        writeExercisesArray(writer, day.getExercises());
-        writer.endObject();
-    }
-
-    private void writeExercisesArray(JsonWriter writer, IExercise[] exercises) throws IOException {
-        writer.beginArray();
-        for (IExercise exercise : exercises){
-            writeExercise(writer, exercise);
-        }
-        writer.endArray();
-    }
-
-    private void writeExercise(JsonWriter writer, IExercise exercise) throws IOException {
-        writer.beginObject();
-        writer.name("name").value(exercise.getName());
-        writer.name("amount").value(exercise.getAmount());
-        writer.name("finished").value(exercise.getFinished());
-        writer.endObject();
-    }
-
-    public void loadFromJSON(){
-
-    }
-
 }
