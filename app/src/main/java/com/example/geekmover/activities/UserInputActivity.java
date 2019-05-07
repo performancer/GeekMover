4384a2d89b0dea34ca4f1dfdbf54a9dc60528ef2
Emil Gusgård <gusemil@hotmail.com>
2019-05-07 23:43:11 +0300

    Back button to UserInputActivity

--- app/src/main/java/com/example/geekmover/activities/UserInputActivity.java
+++ app/src/main/java/com/example/geekmover/activities/UserInputActivity.java
@@ -38,6 +38,8 @@ public class UserInputActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_input);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         pref = getApplicationContext().getSharedPreferences("MyPref", Context.MODE_PRIVATE);
 
         loadData();
